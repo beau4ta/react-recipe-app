6874cd63ce7b9494d0e8c3757725f94d3640178f
Beau J Fortier <beaufortier13@gmail.com>
2022-03-16 22:20:30 -0400

    forgot to commit :(

---
+++ react-recipes/src/App.js
@@ -0,0 +1,14 @@
+import Category from "./components/Category";
+import Pages from "./pages/Pages";
+
+
+function App() {
+  return (
+    <div className="App">
+      <Category />
+     <Pages />
+    </div>
+  );
+}
+
+export default App;
